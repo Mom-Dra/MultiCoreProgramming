8a9fe7d8eb13a7dda8a06758420e44a4db2eba32
MomDra <yem9087@gmail.com>
2026-07-30 16:27:27 +0900

    std::scoped_lock을 활용한 교착 상태 방지 구현

---
+++ MultiCoreProgramming/MultiCoreProgramming/Deadlock.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+
+class Account
+{
+public:
+    int id;
+    int balance;
+    std::mutex m;
+
+    Account(int id, int balance) : id(id), balance(balance) {}
+};
+
+void transfer(Account& from, Account& to, int amount)
+{
+    std::scoped_lock<std::mutex, std::mutex> lock{ from.m, to.m };
+
+    from.balance -= amount;
+    to.balance += amount;
+}
+
+int deadlock()
+{
+    Account acc1{ 1, 10000 };
+    Account acc2{ 2, 10000 };
+
+    // 쓰레드 1: acc1 -> acc2로 1원씩 10000번 이체
+    std::thread t1{ [&]() {
+        for (int i{ 0 }; i < 10000; ++i) {
+            transfer(acc1, acc2, 1);
+        }
+        } };
+
+    // 쓰레드 2: acc2 -> acc1로 1원씩 10000번 이체
+    std::thread t2{ [&]() {
+        for (int i{ 0 }; i < 10000; ++i) {
+            transfer(acc2, acc1, 1);
+        }
+        } };
+
+    t1.join();
+    t2.join();
+
+    std::cout << "이체 완료!\n";
+    std::cout << "계좌 1 잔액: " << acc1.balance << "\n";
+    std::cout << "계좌 2 잔액: " << acc2.balance << "\n";
+
+    return 0;
+}
